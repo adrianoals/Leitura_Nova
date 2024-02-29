b25a29e6ec1ee9a92146c5f5b8d0bfa1ee25d1e8
Adriano Lima <dri.limasantos@gmail.com>
2024-02-29 15:26:40 -0300

    up

--- setup/settings.py
+++ setup/settings.py
@@ -30,11 +30,11 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+# DEBUG = True
+# ALLOWED_HOSTS = ['*']
 
-# DEBUG = False
-# ALLOWED_HOSTS = ['3.15.141.27', 'docevitta.leituranova.com.br', 'www.docevitta.leituranova.com.br']
+DEBUG = False
+ALLOWED_HOSTS = ['3.15.141.27', 'docevitta.leituranova.com.br', 'www.docevitta.leituranova.com.br']
 
 
 
