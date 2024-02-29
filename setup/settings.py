a4a95501de47c5469c2c50ba8cf5f4577334c2c5
Adriano Lima <dri.limasantos@gmail.com>
2024-02-29 16:13:51 -0300

    up

--- setup/settings.py
+++ setup/settings.py
@@ -135,8 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
