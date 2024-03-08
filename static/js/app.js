e955d622876772b772020ad4b4551edf5e1feb4e
Adriano Lima <dri.limasantos@gmail.com>
2024-03-08 13:12:19 -0300

    up

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var viewportMeta = document.querySelector('meta[name="viewport"]');
-    document.querySelectorAll('input, select, textarea').forEach(function(el) {
-        el.addEventListener('focus', function() {
-            viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
-        });
-        el.addEventListener('blur', function() {
-            viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
-        });
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var viewportMeta = document.querySelector('meta[name="viewport"]');
+//     document.querySelectorAll('input, select, textarea').forEach(function(el) {
+//         el.addEventListener('focus', function() {
+//             viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+//         });
+//         el.addEventListener('blur', function() {
+//             viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+//         });
+//     });
+// });
