f0c9e041541007ec703a7ac3dc752226f06fc814
Adriano Lima <dri.limasantos@gmail.com>
2024-04-04 17:24:31 -0300

    up

--- tres_coelho/views.py
+++ tres_coelho/views.py
@@ -1,7 +1,3 @@
-# from django.shortcuts import render
-
-# def tres_coelho(request):
-#     	return render(request, 'tres_coelho/tres_coelho.html')
 
 from django.shortcuts import render, redirect
 from .models import Apartamento, Leitura
@@ -12,31 +8,35 @@ import zipfile
 from django.http import HttpResponse
 from django.conf import settings
 
-def tc_download_photos(request):
-    # Diretório onde as fotos estão armazenadas
-    photos_directory = os.path.join(settings.MEDIA_ROOT, 'leituras/tres_coelho/03')
-    
-    # Lista de caminhos completos para todas as fotos
-    photo_paths = [os.path.join(photos_directory, photo) for photo in os.listdir(photos_directory)]
-    
-    # Configurar um objeto ZipFile em memória
-    memory_zip = zipfile.ZipFile('photos.zip', 'w', zipfile.ZIP_DEFLATED)
+# def doce_vitta(request):
+#     if request.method == 'POST':
+#         apartamento_id = request.POST.get('apartamento')
+#         valor_leitura = request.POST.get('valor_leitura')
+#         foto_relogio = request.FILES.get('foto_relogio')
 
-    # Adicionar todas as fotos ao arquivo zip
-    for photo_path in photo_paths:
-        memory_zip.write(photo_path, os.path.basename(photo_path))
+#         try:
+#             if not all([apartamento_id, valor_leitura, foto_relogio]):
+#                 raise ValueError('Todos os campos são obrigatórios.')
 
-    # Fechar o arquivo zip
-    memory_zip.close()
+#             apartamento = Apartamento.objects.get(id=apartamento_id)
+#             Leitura.objects.create(
+#                 apartamento=apartamento,
+#                 valor_leitura=valor_leitura,
+#                 data_leitura=datetime.date.today(),  # Supondo que a data da leitura é sempre o dia atual
+#                 foto_relogio=foto_relogio
+#             )
+#             messages.success(request, 'Leitura enviada com sucesso!')
+#             return redirect('/ok')
+#         except Exception as e:
+#             # Aqui você pode definir uma mensagem de erro com base na exceção
+#             # e passá-la para a página de erro usando a sessão
+#             request.session['error_message'] = str(e)  # Usando sessão
 
-    # Criar uma resposta HTTP com o arquivo zip em memória
-    response = HttpResponse(open('photos.zip', 'rb'), content_type='application/zip')
-    response['Content-Disposition'] = 'attachment; filename=photos.zip'
+#     # Se não for uma solicitação POST, exiba a página normalmente
+#     blocos = Bloco.objects.all()
+#     apartamentos = Apartamento.objects.all()
+#     return render(request, 'doce_vitta/doce_vitta.html', {'blocos': blocos, 'apartamentos': apartamentos})
 
-    # Remover o arquivo zip em memória após o envio
-    os.remove('photos.zip')
-
-    return response
 
 def dv_download_photos(request):
     # Caminho base onde as fotos estão armazenadas
@@ -57,5 +57,4 @@ def dv_download_photos(request):
                 # Aqui usamos o path relativo para evitar estruturas de diretório absolutas dentro do zip
                 memory_zip.write(file_path, os.path.relpath(file_path, base_directory))
 
-    # Não é necessário remover o arquivo zip, pois ele é criado em memória e enviado diretamente na resposta
     return response
