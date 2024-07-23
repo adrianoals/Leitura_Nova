02c9e6df248c419ef9671b7393252258d3d84d20
Adriano Lima <dri.limasantos@gmail.com>
2024-07-23 11:41:22 -0300

    up

--- tres_coelho/urls.py
+++ tres_coelho/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from tres_coelho.views import tres_coelho, tc_download_photos
 
 urlpatterns = [
-    # path('tres-coelho', tres_coelho, name='tres_coelho'),
+    path('tres-coelho', tres_coelho, name='tres_coelho'),
     path('tc-download', tc_download_photos, name='tc_download_photos'),
 ]
 
