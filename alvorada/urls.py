39bf09e6ba6cfb7521d9273d399c7c561c78d288
Adriano Lima <dri.limasantos@gmail.com>
2024-07-25 16:08:58 -0300

    up

--- alvorada/urls.py
+++ alvorada/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from alvorada.views import imperial, alvorada_download_photos
-
+from alvorada.views import imperial, imperial_download_photos
 urlpatterns = [
         path('imperial', imperial, name='imperial'), 
         path('dv-imperial', imperial_download_photos, name='imperial_download_photos'),
