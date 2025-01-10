a71e73888708d9dda02fdb989183917ca274804a
Adriano Lima <dri.limasantos@gmail.com>
2025-01-10 17:04:01 -0300

    UP

--- imperial/urls.py
+++ imperial/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from imperial.views import imperial, imperial_download_photos, ok
 urlpatterns = [
-        # path('imperial', imperial, name='imperial'), 
+        path('imperial', imperial, name='imperial'), 
         path('dw-imperial', imperial_download_photos, name='imperial_download_photos'),
         path('ok', ok, name='ok'),
 ]
