62ba2b1f72a3321bae6412aac86c1fdb2f48b628
Adriano Lima <dri.limasantos@gmail.com>
2025-05-27 12:03:00 -0300

    up

--- alvorada/urls.py
+++ alvorada/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from alvorada.views import alvorada, alvorada_download_photos, ok
 urlpatterns = [
-        path('alvorada', alvorada, name='alvorada'), 
+        # path('alvorada', alvorada, name='alvorada'), 
         path('dw-alvorada', alvorada_download_photos, name='alvorada_download_photos'),
         path('ok', ok, name='ok'),
 ]
