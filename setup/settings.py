7f2c7424a6f5e061117adbb4ee2e4448ed6fad27
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2025-06-04 16:04:17 -0300

    update

--- setup/settings.py
+++ setup/settings.py
@@ -172,8 +172,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
