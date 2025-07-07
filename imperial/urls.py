17f3be65bcfa175f10d63e05981a6e646a84b5db
Adriano Lima <dri.limasantos@gmail.com>
2025-07-07 16:16:43 -0300

    up

--- imperial/urls.py
+++ imperial/urls.py
@@ -3,7 +3,7 @@ from imperial.views import imperial, imperial_download_photos
 from . import views
 
 urlpatterns = [
-        path('imperial', imperial, name='imperial'), 
+        # path('imperial', imperial, name='imperial'), 
         path('dw-imperial', imperial_download_photos, name='imperial_download_photos'),
         path('imperial/excel', views.DownloadExcelView.as_view(), name='download_excel'),
 ]
