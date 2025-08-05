48f0448ec8354169f8d3a73f120466b0c50e004a
Adriano Lima <dri.limasantos@gmail.com>
2025-08-05 15:04:57 -0300

    up_alvorada

--- alvorada/urls.py
+++ alvorada/urls.py
@@ -3,7 +3,7 @@ from alvorada.views import alvorada, alvorada_download_photos
 from . import views
 
 urlpatterns = [
-        # path('alvorada', alvorada, name='alvorada'), 
+        path('alvorada', alvorada, name='alvorada'), 
         path('dw-alvorada', alvorada_download_photos, name='alvorada_download_photos'),
         path('alvorada/excel', views.DownloadExcelView.as_view(), name='download_excel'),
 ]
